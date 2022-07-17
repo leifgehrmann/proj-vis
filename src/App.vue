0fe8b6490d708a2f84c08629d79314591b3a81c1
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-07-17 23:06:18 +0100

    Added image texture to point projection.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,31 @@
 import './index.css'
 import { ref, onMounted } from 'vue'
 import proj4 from 'proj4'
-const projection = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs '
+import seaLandImage from './assets/sea-land.png'
 
-onMounted(() => {
+let projection = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs '
+function loadImage(imgElement: HTMLImageElement): Promise<void> {
+  return new Promise((resolve, reject) => {
+    imgElement.onload = () => {
+      resolve()
+    }
+  })
+}
+
+onMounted(async () => {
+  const inputMapImg = document.querySelector('#inputMap') as HTMLImageElement;
+
+  console.log(inputMapImg.src)
+
+  await loadImage(inputMapImg)
+
+  const inputMapCanvas = document.createElement('canvas');
+  const inputMapCtx = inputMapCanvas.getContext('2d');
+
+  inputMapCanvas.width = inputMapImg.naturalWidth;
+  inputMapCanvas.height = inputMapImg.naturalHeight;
+  inputMapCtx.drawImage(inputMapImg, 0, 0);
+
   const canvas2 = document.getElementById("myCanvas2") as HTMLCanvasElement|null;
   if (canvas2 === null) {
     return
@@ -14,7 +36,7 @@ onMounted(() => {
     return
   }
   ctx2.fillStyle = "#000000";
-  ctx2.fillRect(0, 0, 400, 400);
+  ctx2.fillRect(0, 0, 800, 800);
 
   const canvas = document.getElementById("myCanvas") as HTMLCanvasElement|null;
   if (canvas === null) {
@@ -24,14 +46,16 @@ onMounted(() => {
   if (ctx === null) {
     return
   }
+
   ctx.fillStyle = "#000000";
-  ctx.fillRect(0, 0, 400, 400);
+  ctx.fillRect(0, 0, 1000, 1000);
 
   const transformer = proj4(projection);
   const xValues = []
   const yValues = []
   const validLons = []
   const validLats = []
+  const inputMapColors = []
 
   const minLatRange = -90
   const maxLatRange = 90
@@ -51,21 +75,32 @@ onMounted(() => {
       yValues.push(value[1])
       validLons.push(lon)
       validLats.push(lat)
+      const inputMapImgX = Math.round((lon + 180)/360 * inputMapImg.naturalWidth)
+      const inputMapImgY = Math.round((-lat + 90)/180 * inputMapImg.naturalHeight)
+      inputMapColors.push(inputMapCtx.getImageData(
+          inputMapImgX, inputMapImgY, 1, 1
+      ).data)
     }
   }
-  const minX = xValues.reduce((a, b) => Math.min(a, b), Number.POSITIVE_INFINITY)
-  const maxX = xValues.reduce((a, b) => Math.max(a, b), Number.NEGATIVE_INFINITY)
+
+  if (xValues.length == 0) {
+    console.log('No values projected')
+    return
+  }
+
+  const minX = xValues.reduce((a, b) => Math.min(a, b))
+  const maxX = xValues.reduce((a, b) => Math.max(a, b))
   const dX = maxX - minX
-  const minY = yValues.reduce((a, b) => Math.min(a, b), Number.POSITIVE_INFINITY)
-  const maxY = yValues.reduce((a, b) => Math.max(a, b), Number.NEGATIVE_INFINITY)
+  const minY = yValues.reduce((a, b) => Math.min(a, b))
+  const maxY = yValues.reduce((a, b) => Math.max(a, b))
   const dY = maxY - minY
 
   const ratio = dX / dY
-  let rWidth = 400
-  let rHeight = 400 / ratio
+  let rWidth = 1000
+  let rHeight = 1000 / ratio
   if (ratio < 1) {
-    rWidth = 400 * ratio
-    rHeight = 400
+    rWidth = 1000 * ratio
+    rHeight = 1000
   }
 
   const minLon = validLons.reduce((a, b) => Math.min(a, b))
@@ -75,19 +110,20 @@ onMounted(() => {
   const maxLat = validLats.reduce((a, b) => Math.max(a, b))
   const dLat = maxLat - minLat
 
-
   for (let i = 0; i < xValues.length; i++) {
     const roundedX = Math.round((xValues[i] - minX) / (dX) * rWidth)
-    const roundedY = Math.round(rHeight-(yValues[i] - minY) / (dY) * rHeight)
-    ctx.fillStyle = `rgb(${((validLats[i] + 90) / 180 * 255)}, ${((validLons[i] + 180) / 360 * 255)},0)`;
-    ctx.fillRect(roundedX, roundedY, 1, 1);
+    const roundedY = Math.round((rHeight-(yValues[i] - minY) / (dY) * rHeight))
+    const color = inputMapColors[i]
+    ctx.fillStyle = `rgb(${color[0]},${color[1]},${color[2]})`;
+    ctx.fillRect(roundedX, roundedY, 2, 2);
   }
 
   for (let i = 0; i < validLons.length; i++) {
-    const x = (validLons[i] - minLon) / (dLon) * (maxLonRange - minLonRange) / dxLonRange
-    const y = (validLats[i] - minLat) / (dLat) * (maxLatRange - minLatRange) / dxLatRange
-    ctx2.fillStyle = `rgb(${((validLats[i] + 90) / 180 * 255)}, ${((validLons[i] + 180) / 360 * 255)},0)`;
-    ctx2.fillRect(x, y, 1, 1);
+    const x = ((validLons[i] - minLon) / (dLon) * (maxLonRange - minLonRange) / dxLonRange) * 2
+    const y = ((-validLats[i] - minLat) / (dLat) * (maxLatRange - minLatRange) / dxLatRange) * 2
+    const color = inputMapColors[i]
+    ctx2.fillStyle = `rgb(${color[0]},${color[1]},${color[2]})`;
+    ctx2.fillRect(x, y, 2, 2);
   }
 })
 
@@ -121,13 +157,19 @@ onMounted(() => {
           <p :class="{invisible: false}">Too many samples: 6,480,000 (3,600 × 1,800). <br>Maximum is 1,000,000 samples. </p>
         </div>
       </div>
-      <div class="my-2 border-2 border-white aspect-[2/1] bg-black">
+      <div class="my-2 border-2 border-white bg-black">
+        <img
+          id="inputMap"
+          alt="The world map"
+          class="w-full aspect-[2/1]"
+          :src="seaLandImage"
+        >
       </div>
       <div class="my-2 border-2 border-white w-full aspect-[2/1] bg-black">
-        <canvas id="myCanvas2" class="w-full" width="400px" height="200px"></canvas>
+        <canvas id="myCanvas2" class="w-full" width="800" height="800"></canvas>
       </div>
       <div class="my-2 border-2 border-white aspect-[1/1] bg-black">
-        <canvas id="myCanvas" class="w-full" width="400px" height="400px"></canvas>
+        <canvas id="myCanvas" class="w-full" width="1000" height="1000"></canvas>
       </div>
     </article>
     <footer class="sm:p-10 text-center">
