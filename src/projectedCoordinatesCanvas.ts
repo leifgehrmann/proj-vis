a36a2c255574d1bde3fb30e520c7273d500e9fa7
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-07-24 21:20:43 +0100

    Add padding of 1 px to projectedCoordinatesCanvas to account for 2x2 rectangle size

--- src/projectedCoordinatesCanvas.ts
+++ src/projectedCoordinatesCanvas.ts
@@ -10,6 +10,7 @@ export function createProjectedCoordinatesCanvas(
   const dY = bbox.maxY - bbox.minY
 
   const ratio = dX / dY
+  const padding = 1
   let rWidth = 1000
   let rHeight = 1000 / ratio
   if (ratio < 1) {
@@ -27,8 +28,8 @@ export function createProjectedCoordinatesCanvas(
   }
 
   for (let i = 0; i < xValues.length; i++) {
-    const x = (xValues[i] - bbox.minX) / (dX) * rWidth
-    const y = (rHeight-(yValues[i] - bbox.minY) / (dY) * rHeight)
+    const x = (xValues[i] - bbox.minX) / (dX) * (rWidth - padding * 2) + 1
+    const y = ((rHeight - padding * 2)-(yValues[i] - bbox.minY) / (dY) * (rHeight - padding * 2)) + 1
     const color = colorMap[i]
     ctx.fillStyle = `rgb(${color[0]},${color[1]},${color[2]})`;
     ctx.fillRect(x-1, y-1, 2, 2);
