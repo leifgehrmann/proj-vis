fa41a29e8f25457025a16054aea4ed10bd8176d1
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-08-18 23:13:10 +0100

    The coordinate is now displayed on the map on click

--- src/components/ProjectedCoordinates.vue
+++ src/components/ProjectedCoordinates.vue
@@ -18,14 +18,13 @@
       }"
     ></div>
   </div>
-  <div class="text-center">&nbsp;</div>
+  <div class="text-center">{{debug}}</div>
 </template>
 
 <script setup lang="ts">
 import {computed, onMounted, ref, watch} from "vue";
 import {createProjectedCoordinatesCanvasAndTransformers} from "../projectedCoordinatesCanvas";
 import {Coordinate} from "../coord";
-import {getBbox} from "../bbox";
 
 const props = defineProps<{
   xValues: number[],
@@ -41,44 +40,13 @@ const canvasToCoord = ref<((c: Coordinate) => Coordinate)|null>(null)
 const coordToCanvas = ref<((c: Coordinate) => Coordinate)|null>(null)
 const clientToCanvas = ref<((c: Coordinate) => Coordinate)|null>(null)
 const canvasToClient = ref<((c: Coordinate) => Coordinate)|null>(null)
-
-const bbox = computed(() => {
-  return getBbox(props.xValues, props.yValues)
-})
+const clientToOffset = ref<((c: Coordinate) => Coordinate)|null>(null)
+const debug = ref<string>('')
 
 const markerCoordinateModel = computed({
   get() { return props.markerCoordinate },
   set(markerCoordinate) { emit('update:markerCoordinate', markerCoordinate) }
 })
-
-function convertMousePositionToCanvasPosition(
-    container: HTMLDivElement,
-    canvas: HTMLCanvasElement,
-    event: MouseEvent
-): Coordinate {
-  const containerBounds = container.getBoundingClientRect()
-  return {
-    x: (event.clientX - containerBounds.left) / containerBounds.width,
-    y: (event.clientY - containerBounds.top) / containerBounds.height
-  }
-}
-
-function convertMousePositionToCoordinatePosition(
-    container: HTMLDivElement,
-    canvas: HTMLCanvasElement,
-    event: MouseEvent
-): Coordinate {
-  const padding = 1
-  const topLeftCoordinate = {
-    x: bbox.value.
-    y:
-  }
-  const canvasBounds = canvas.getBoundingClientRect()
-  return {
-    x: (event.clientX - canvasBounds.left) / canvasBounds.width,
-    y: (event.clientY - canvasBounds.top) / canvasBounds.height
-  }
-}
 
 watch(() => [props.xValues, props.yValues, props.colorValues], async ([xValues, yValues, colorValues]) => {
   const containerElement = container.value
@@ -92,7 +60,7 @@ watch(() => [props.xValues, props.yValues, props.colorValues], async ([xValues, 
   }
 
   // Type definitions need to be declared because for some reason `watch` does not care about array order.
-  const {canvas} = createProjectedCoordinatesCanvasAndTransformers(
+  const {canvas, toCanvas, toCoords} = createProjectedCoordinatesCanvasAndTransformers(
       xValues as number[],
       yValues as number[],
       colorValues as Uint8ClampedArray[]
@@ -103,6 +71,34 @@ watch(() => [props.xValues, props.yValues, props.colorValues], async ([xValues, 
   canvas.classList.add('h-full')
 
   containerElement.replaceChildren(canvas)
+
+  canvasToCoord.value = toCoords
+  coordToCanvas.value = toCanvas
+  clientToCanvas.value = (coordinate: Coordinate): Coordinate => {
+    const canvasWidth = canvas.width
+    const canvasHeight = canvas.height
+    const containerBounds = canvas.getBoundingClientRect()
+    return {
+      x: ((coordinate.x - containerBounds.left) / containerBounds.width) * canvasWidth,
+      y: ((coordinate.y - containerBounds.top) / containerBounds.height) * canvasHeight
+    }
+  }
+  canvasToClient.value = (coordinate: Coordinate): Coordinate => {
+    const canvasWidth = canvas.width
+    const canvasHeight = canvas.height
+    const containerBounds = canvas.getBoundingClientRect()
+    return {
+      x: ((coordinate.x / canvasWidth) * containerBounds.width) + containerBounds.left,
+      y: ((coordinate.y / canvasHeight) * containerBounds.height) + containerBounds.top
+    }
+  }
+  clientToOffset.value = (coordinate: Coordinate): Coordinate => {
+    const containerBounds = containerElement.getBoundingClientRect()
+    return {
+      x: (coordinate.x - containerBounds.left) / containerBounds.width,
+      y: (coordinate.y - containerBounds.top) / containerBounds.height
+    }
+  }
 })
 
 onMounted(() => {
@@ -119,11 +115,17 @@ onMounted(() => {
     if (canvasElement === null) {
       return
     }
-    markerCoordinateModel.value = convertMousePositionToCanvasPosition(
-        containerElement,
-        canvasElement,
-        event
+    if (
+        clientToOffset.value === null ||
+        clientToCanvas.value === null ||
+        canvasToCoord.value === null
+    ) {
+      return
+    }
+    markerCoordinateModel.value = clientToOffset.value(
+        {x: event.clientX, y: event.clientY}
     )
+    debug.value = JSON.stringify(canvasToCoord.value(clientToCanvas.value({x: event.clientX, y: event.clientY})))
   })
 })
 </script>
